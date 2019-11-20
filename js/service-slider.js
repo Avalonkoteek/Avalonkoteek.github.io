9a746c0c61fb0e916525c1869996a5b70c29f07d
Bykov Dmitry <fepifmel@gmail.com>
2019-11-21 00:24:34 +0300

    изменено название классов

--- js/service-slider.js
+++ js/service-slider.js
@@ -1,10 +1,10 @@
 $(document).ready(function($) {
-  let serviceContentWrapper = $(".service-content__wrapper");
+  let serviceContentWrapper = $(".js-slider-wrapper");
 
   serviceContentWrapper.each(function() {
     let blockNavigation = $(this).find(".js-btn-wrapper");
 
-    let imageWrapper = $(this).find(".service-content__face1");
+    let imageWrapper = $(this).find(".js-image-wrapper");
     let imagesList = imageWrapper.children(".js-visual-content");
 
     let textWrapper = $(this).find(".service-content__face2");
@@ -23,38 +23,34 @@ $(document).ready(function($) {
       }
     );
 
-    $(this).on(
-      "click",
-      ".service-content__face1 .js-btn-wrapper button",
-      function() {
-        let direction = $(this);
+    $(this).on("click", ".js-image-wrapper .js-btn-wrapper button", function() {
+      let direction = $(this);
 
-        let indexVisibleblock = imagesList.index(
-          imageWrapper.children(".is-selected")
-        );
+      let indexVisibleblock = imagesList.index(
+        imageWrapper.children(".is-selected")
+      );
 
-        if (indexVisibleblock == undefined) {
-          imagesList[0].addClass(".is-selected");
-          indexVisibleblock = 0;
-        }
-        
-        let index = indexVisibleblock;
-
-        if (!direction.hasClass("inactive")) {
-          if (direction.hasClass("cd-next")) {
-            index++;
-          } else {
-            index--;
-          }
-          let imageItem = imagesList.eq(index);
-          let textItem = textList.eq(index);
-          let imageLength = imagesList.length;
-          classUpdate(imageItem);
-          classUpdate(textItem);
-          updateBlockNavigation(index, blockNavigation, imageLength);
-        }
+      if (indexVisibleblock == undefined) {
+        imagesList[0].addClass(".is-selected");
+        indexVisibleblock = 0;
       }
-    );
+
+      let index = indexVisibleblock;
+
+      if (!direction.hasClass("inactive")) {
+        if (direction.hasClass("cd-next")) {
+          index++;
+        } else {
+          index--;
+        }
+        let imageItem = imagesList.eq(index);
+        let textItem = textList.eq(index);
+        let imageLength = imagesList.length;
+        classUpdate(imageItem);
+        classUpdate(textItem);
+        updateBlockNavigation(index, blockNavigation, imageLength);
+      }
+    });
   });
 
   function updateBlockNavigation(n, blockNavigation, imageLength) {
