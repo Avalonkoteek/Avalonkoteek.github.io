9f1c6f77e7eb5628161df4feffe8f9bd83323042
Bykov Dmitry <fepifmel@gmail.com>
2019-11-21 23:51:38 +0300

    json

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,13 +1,16 @@
-const requestURL = "https://avalonkoteek.github.io/portfolio/data.json";
+const requestURL =
+  "http://127.0.0.1:5500/VideoServise-website/portfolio/data.json";
+
+sendRequest("GET", requestURL)
+  .then(data => addItem(data))
+  .catch(err => console.log(err));
 
 function sendRequest(method, url) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
+
     xhr.open(method, url);
     xhr.responseType = "json";
-
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Access-Control-Allow-Origin: *");
 
     xhr.onload = () => {
       if (xhr.status >= 400) {
@@ -19,16 +22,12 @@ function sendRequest(method, url) {
     xhr.onerror = () => {
       reject(xhr.response);
     };
+
+    xhr.send();
   });
 }
-function addItem(data) {
-  let name_first = data[0].id;
-  console.log(name_first);
-}
-sendRequest("GET", requestURL)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
 
-// sendRequest('POST', requestURL,body)
-// .then(data=> console.log(data))
-// .catch(err => console.log(err));
+function addItem(data) {
+  const videoArr = data.filter(element => element.type == "video");
+  console.log(videoArr);
+}
