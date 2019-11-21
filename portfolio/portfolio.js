23dc2e294eae5c0f620ef9b32ce53cc0b5f0209f
Bykov Dmitry <fepifmel@gmail.com>
2019-11-21 19:44:53 +0300

    истоки

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,42 +1,33 @@
-const requestURL = 'https://avalonkoteek.github.io/portfolio/data.json';
+const requestURL = "https://avalonkoteek.github.io/portfolio/data.json";
 
-function sendRequest(method, url){
-    return new Promise((resolve, reject)=>{
+function sendRequest(method, url) {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    
-    xhr.open(method,url);                         //open new request(method(get-получение,post-добавление,put,puch...))
-    
-    xhr.responseType ="json";  
+    xhr.open(method, url);
+    xhr.responseType = "json";
 
-    xhr.setRequestHeader('Content-Type','application/json')
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Access-Control-Allow-Origin: *");
 
-    xhr.onload = () =>{
-        if(xhr.status >=400){
-            reject(xhr.response);
-        }                                               //обработка всех ошибок
-      resolve(xhr.response);                            //если успешный ответ от сервера
-    }
-    
-    xhr.onerror =() =>{
-        reject(xhr.response);   //если ошибка
-    }
-    
-    });
-    
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      }
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = () => {
+      reject(xhr.response);
+    };
+  });
 }
-function addItem(data){
-    let name_first = data[0].id;
-    console.log(name_first);
-} 
-sendRequest('GET', requestURL)
-.then(data => {
-    let arr=[];
-    data.forEach(element => {
-        arr.push(element);
-    });
-    addItem(arr);
-})
-.catch(err => console.log(err));
+function addItem(data) {
+  let name_first = data[0].id;
+  console.log(name_first);
+}
+sendRequest("GET", requestURL)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
 
 // sendRequest('POST', requestURL,body)
 // .then(data=> console.log(data))
