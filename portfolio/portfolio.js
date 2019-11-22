07f6d74310948fca4aa3739d7342cd98a83d9976
Быков Дмитрий <fepifmel@gmail.com>
2019-11-22 14:38:12 +0300

    описан скрипт по формированию контента в портфолио

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,8 +1,8 @@
 const requestURL =
-  "http://127.0.0.1:5500/VideoServise-website/portfolio/data.json";
+  "http://127.0.0.1:5500/portfolio/data.json";
 
 sendRequest("GET", requestURL)
-  .then(data => addItem(data))
+  .then(data => addContent(data))
   .catch(err => console.log(err));
 
 function sendRequest(method, url) {
@@ -29,13 +29,76 @@ function sendRequest(method, url) {
 
 function addItem(data) {
   const videoArr = data.filter(element => element.type == "video");
-  console.log(videoArr);
+  console.log(data);
+  checkedTab();
 
-  $("input[name='radio-buttons-group']").change(function() {
-    if ($("input[value='kn1']").prop("checked")) {
-        $('.block').css("display", "block");  // Этот код сработает
-    } else {
-        $('.block').css("display", "none");  // И этот тоже
-    }
+}
+function checkedTab(){
+  let $tabList = $(".portfolio-navigation__tab");
+  $tabList.each(function(){
+ if($(this).data("checked")=="checked" ){
+  uppdateContent($(this).data("type"));
+ };
+  })
+}
+function sortData(dataArr,type){
+  let newData = dataArr.filter(obj => {
+    let ar = obj.type.filter(s => {return s.visualType === type});
+    if(ar.length == 0)return true;
+    else return false;
+  });
+  return newData;
+
+}
+function addContent(SortData){
+  SortData.forEach(element => {
+    createElement(element);
+  });
+}
+
+
+function createElement(element){
+
+let title = element.title;
+let decription = element.text;
+let price = element.price;
+let time = element.time;
+let format = element.format;
+
+// base blocks
+let $ul__wrapper = $(".js-portfolio-wrapper");
+let $li = $('<li class="portfolio-item"></li>');
+let $visualContentWrapper = $('<div class="portfolio-item__visual-box"></div>');
+let $textContentWrapper = $('<div class="portfolio-item__text-box"></div>');
+
+let $textContentItem__title = $('<h3 class="portfolio-item__title">'+title+'</h3>');
+let $textContentItem__description = $('<div class="portfolio-item__description">'+decription+'</div>');
+
+$ul__wrapper.append($li);
+$li.append($visualContentWrapper);
+$li.append($textContentWrapper);
+
+$textContentWrapper.append($textContentItem__title);
+$textContentWrapper.append($textContentItem__description);
+
+// text
+
+
+//visual
+element.type.forEach(el =>{
+  let type = el.visualType;
+  if(type=="video"){
+    let $visualContentItem = $('<div class="portfolio-item__visual-item">VIDEO'+el.url+'</div>');
+    $visualContentWrapper.append($visualContentItem);
+  }
 });
+// слайдер
+// video
+//  
+
+}
+class PortfolioSlider{
+ constructor(arr){
+   
+ }
 }
