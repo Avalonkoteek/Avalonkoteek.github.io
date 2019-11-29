43c09a30a5bf9969fe8ad83732c1681af90ce360
Быков Дмитрий <fepifmel@gmail.com>
2019-11-29 12:58:35 +0300

    добавлена полная стилизация главной страницы, починил скрипт меню, изменил контакты

--- src/js/sticky-nav.js
+++ src/js/sticky-nav.js
@@ -1,20 +1,11 @@
 class StickyNavigation {
   constructor() {
-    this.currentId = null;
-    this.currentTab = null;
+  
     this.tabContainerHeight = -60;
     let self = this;
     const burger = $(".burger");
     const nav = $(".nav-links");
 
-    // $(".js-nav-link").click(function() {
-    //   self.onLinkClick(event, $(this));
-
-    //   if (burger.hasClass("toggle")) {
-    //     burger.removeClass("toggle");
-    //     nav.removeClass("nav-active");
-    //   }
-    // });
 
     $(window).scroll(() => {
       this.onScroll();
@@ -31,19 +22,12 @@ class StickyNavigation {
     });
   }
 
-  onLinkClick(event, element) {
-    event.preventDefault();
-    let scrollTop =
-      $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
-    $("html, body").animate({ scrollTop: scrollTop }, 600);
-  }
-
   onScroll() {
     this.checkTabContainerPosition();
   }
 
   checkTabContainerPosition() {
-    let offset = $(".header-main").offset().top;
+    let offset = $("header").offset().top;
     if ($(window).scrollTop() > offset) {
       $(".js-nav").addClass("js-nav--top");
     } else {
